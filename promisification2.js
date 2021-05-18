334842023afb6cf7f8c6da9461370624534bb8ce
Agarkovsergey <sergeyagarkov91@mail.ru>
2021-05-18 17:23:11 +0300

    hw11 update

--- promisification2.js
+++ promisification2.js
@@ -6,14 +6,23 @@ const users = [
     { name: 'Lux', active: false },
 ];
 
-const  toggleUserState = async (allUsers, userName) => {
-    const updatedUsers = allUsers.map(user =>
-        user.name === userName ? { ...user, active: !user.active } : user,
-    );
-    return updatedUsers
+// const  toggleUserState = async (allUsers, userName) => {
+//     const updatedUsers = allUsers.map(user =>
+//         user.name === userName ? { ...user, active: !user.active } : user,
+//     );
+//     return updatedUsers
+// };
+
+const toggleUserState = (allUsers, userName) => {
+    return new Promise((res) => {
+        const updatedUsers = allUsers.map(user =>
+            user.name === userName ? { ...user, active: !user.active } : user,
+        );
+        res(updatedUsers)
+    })
+    
 
 };
-
 const logger = updatedUsers => console.table(updatedUsers);
 
 
